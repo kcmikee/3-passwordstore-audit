c193957b1bdbe2ecd823419def61ed0825c5223e
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2025-07-18 10:48:00 +0100

    finding. 1

---
+++ findings_layout.md
@@ -0,0 +1,13 @@
+<!-- Convince the protocol this is an issue -->
+<!-- Explain to them how bad it is -->
+<!-- Explain to them how to fix the issue -->
+
+### [S-#] TITLE (Root Cause + Impact)
+
+**Description:**
+
+**Impact:**
+
+**Proof of Concept:**
+
+**Recommended Mitigation:**
--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.18;
 /*
  * @author not-so-secure-dev
  * @title PasswordStore
- * @notice This contract allows you to store a private password that others won't be able to see. 
+ * @notice This contract allows you to store a private password that others won't be able to see.
  * You can update your password at any time.
  */
 contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit s_password is not actually private! This is not a safe place to save your password
     string private s_password;
 
     event SetNewPassword();
@@ -23,6 +24,11 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
+    // q can a non owner set a password
+    //  q should a non-owner be able to set a password
+    // @audit any user can set a password
+    // missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNewPassword();
@@ -30,6 +36,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     * @audit there is no newPassword parameter!
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
